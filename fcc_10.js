7628f752533123425838253ce6fc0c3fd03681c7
Denis Shubin <denis.shubin@gmail.com>
2022-01-16 14:42:30 +0300

    My solve

--- fcc_10.js
+++ fcc_10.js
@@ -1,4 +1,4 @@
-var fCC = "freeCodeCamp"; // Change this line
-var fact = "is cool!"; // Change this line
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
 fact = "is awesome!";
-console.log(fCC, fact); // Change this line
+console.log(FCC, fact); // Change this line
