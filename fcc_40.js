760c0d9b8955ab7f7c311bacdaf8a99c24fdb57c
Denis Shubin <denis.shubin@gmail.com>
2022-01-18 17:28:02 +0300

    added new file

---
+++ fcc_40.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+    ["Chicago Bulls", 23],
+    ["Los Angeles Lakers", 32]
+];
---
+++ fcc_48.js
@@ -0,0 +1,7 @@
+const myList = [
+    ["Bread", 3],
+    ["Butter", 2],
+    ["Cheese", 2],
+    ["Sausage", 1],
+    ["Milk", 2]
+];
